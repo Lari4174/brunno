a517d41dc8ae4348f82c1290bf21958de76c5fd8
Lari4174 <116530633+Lari4174@users.noreply.github.com>
2022-11-28 03:34:10 -0300

    Update atp.sql

--- base de dados/atp.sql
+++ base de dados/atp.sql
@@ -20,7 +20,8 @@ SET time_zone = "+00:00";
 --
 -- Banco de dados: `atp`
 --
-
+CREATE DATABASE IF NOT EXISTS `atp` DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;
+USE `atp`;
 -- --------------------------------------------------------
 
 --
